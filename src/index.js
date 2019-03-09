a36cea4d5b94094b4fea57520d9708882a40fe46
Xiaochan Xia <xiaochan.xia@qq.com>
2019-03-09 22:28:38 +0800

    feat: change to config-angular

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 module.exports = {
-  extends: '@commitlint/config-conventional',
+  extends: '@commitlint/config-angular',
   rules: {
+    "subject-case": [0],
+    "scope-max-length": [1, "always", 20]
   },
 };
