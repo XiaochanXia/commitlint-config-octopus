ef09e94cae08f9fb702045a9b8d5527ae57aaeca
Xiaochan Xia <xiaochan.xia@qq.com>
2019-03-11 22:49:39 +0800

    feat: add rule comments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'subject-case': [0],
+    'subject-case': [0], // subject ingore case
+    // open scope max-length validation, but the max-length is 20
     'scope-max-length': [1, 'always', 20],
   },
 };
